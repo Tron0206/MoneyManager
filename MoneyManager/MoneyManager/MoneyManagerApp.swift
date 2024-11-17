4f03f3027926d563c4acee6dc0c2d459c0b2f222
Zhasur Sidamatov <sidamatov.offwork@gmail.com>
2024-11-17 17:24:04 +0300

    Create source project file

---
+++ MoneyManager/MoneyManager/MoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagerApp.swift
+//  MoneyManager
+//
+//  Created by Zhasur Sidamatov on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
