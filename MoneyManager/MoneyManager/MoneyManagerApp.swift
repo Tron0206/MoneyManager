c3580fe5164a111253ef570773ade7de6ba6cf7f
an3ks <avdienkodanya99@gmail.com>
2024-11-27 15:06:56 +0300

    Auth and data service

--- MoneyManager/MoneyManager/MoneyManagerApp.swift
+++ MoneyManager/MoneyManager/MoneyManagerApp.swift
@@ -7,18 +7,41 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
+
 @main
 struct MoneyManagerApp: App {
-    init(){
-        FirebaseApp.configure()
-        let tester = FirebaseServiceTester()
-        tester.testRegister()
-        tester.testLogOut()
-        tester.testLogIn()
-    }
+    @StateObject private var dataService = DataService()
+    private let firebaseService = FirebaseAuthService.shared
+    @State private var currentUserId: String?
+        
+    init() {
+            FirebaseApp.configure()
+            print("Firebase configured")
+        }
+    
     var body: some Scene {
-        WindowGroup {
-            EmptyView()
+            WindowGroup {
+                ContentView()
+                    .onAppear {
+                        firebaseService.logIn(email: "test1@example.com", password: "TestPassword123") { result in
+                            switch result {
+                            case .success(let userId):
+                                self.currentUserId = userId
+                                print("Вход выполнен. userId: \(userId)")
+                                // Вызываем функции после сохранения userId
+                                dataService.addExpense(userId: userId, category: "Transport", fee: 1000, date: "2024-11-27")
+                                dataService.fetchExpenses(for: userId)
+                                
+                            case .failure(let error):
+                                print("Ошибка входа: \(error.localizedDescription)")
+                            }
+                        }
+                    }
+            }
         }
     }
-}
+
+
